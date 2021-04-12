a5082b847c6aedf7b8ad9d7ea1dfdc89ac73edfc
erick-adalid <sebas0yatra@gmail.com>
2021-04-12 17:22:44 -0400

    segundo

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
 app = Flask(__name__)
-@app.route('/')
+@app.route('/pagina-web-prueba.herokuapp.com')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World!'h
