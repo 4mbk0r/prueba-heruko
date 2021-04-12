9ff64bc702a243287b2ccff631bfbe059378d1af
erick-adalid <sebas0yatra@gmail.com>
2021-04-12 19:12:44 -0400

    primera aplicaion input

--- app.py
+++ app.py
@@ -7,3 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def index_post():
+    text = request.form['text']
+    return render_template("index.html", respuesta=text )
+    
