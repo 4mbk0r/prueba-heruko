a3cd81243761c811da8d41385bdd027d47acd5c4
erick-adalid <sebas0yatra@gmail.com>
2021-04-19 15:24:41 -0400

    chatbot interfaces

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template
+from flask import request
 
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/')
+
+
+@app.route('/',  methods=["POST", "GET"])
 def index():
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
-def index_post():
-    text = request.form['text']
-    return render_template("index.html", respuesta=text )
-    
+@app.route("/get", methods=["POST"])
+def chatbot_response():
+    msg = request.form["msg"]
+    return msg
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
